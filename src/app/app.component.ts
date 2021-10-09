513d59925e35b138cbf5b4ebcfd4ccc525c09e32
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-09 23:59:05 +0300

    list quote using the quote blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,10 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   title = 'Quotes';
-  
+  quotes : Quote [] = [
+    new Quote (1, 'Brenda Andeso', 'You only live once, but if you do it right, once is enough', 'Mae West',new Date(2021,3,21)),
+    new Quote (2, 'Collins Wasonga','If you want to live a happy life,tie it to a goal, not people or things','Albert Einsten',new Date(2020,7,27)),
+    new Quote (3, 'Ashley Livoga','Never let the fear of striking out keep you from playing the game','Babe Ruth',new Date(2020,9,16)),
+
+  ]
 }
