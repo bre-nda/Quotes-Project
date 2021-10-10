90015830f1c5cb8898c3c37f799718d3796fb45c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-10 22:20:45 +0300

    create quoteComplete function

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -8,6 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteDetailComponent implements OnInit {
   @Input() quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
   constructor() { }
 
   ngOnInit(){
