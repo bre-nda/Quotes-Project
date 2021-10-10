344fffa7489f4548d96464359699bbdbd675706d
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-10 20:48:07 +0300

    add changes to the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   // ];
   quotes:Quote[] = [
     new Quote(1,'You only live once, but if you do it right, once is enough','Mae West','Brenda Andeso'),
-  new Quote(2,'If you want to live a happy life, tie to a goal, not people or things','Albert Eintsen','Collins Wasonga'),
-  new Quote(3,'Never let the fear of striking out keep you from playing the game','Babe Ruth','Ashley Liavoga'),
+    new Quote(2,'If you want to live a happy life, tie to a goal, not people or things','Albert Eintsen','Collins Wasonga'),
+    new Quote(3,'Never let the fear of striking out keep you from playing the game','Babe Ruth','Ashley Liavoga'),
   ];
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,11 +13,14 @@ export class QuoteComponent implements OnInit {
   //   new Quote (3, 'Ashley Livoga','Never let the fear of striking out keep you from playing the game','Babe Ruth',new Date(2020,9,16)),
   // ];
   quotes:Quote[] = [
-  new Quote(1,'You only live once, but if you do it right, once is enough','Mae West','Brenda Andeso'),
-  new Quote(2,'If you want to live a happy life, tie to a goal, not people or things','Albert Eintsen','Collins Wasonga'),
-  new Quote(3,'Never let the fear of striking out keep you from playing the game','Babe Ruth','Ashley Liavoga'),
+    new Quote(1,'You only live once, but if you do it right, once is enough','Mae West','Brenda Andeso'),
+    new Quote(2,'If you want to live a happy life, tie to a goal, not people or things','Albert Eintsen','Collins Wasonga'),
+    new Quote(3,'Never let the fear of striking out keep you from playing the game','Babe Ruth','Ashley Liavoga'),
   
   ];
+  toggleDetails(index:number){
+    this.quotes[index].showPublisher = !this.quotes[index].showPublisher;
+  }
 
   constructor() { }
 
