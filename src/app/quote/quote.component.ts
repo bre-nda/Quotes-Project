21f43ba16799e1077b04ff4d33a248aec83ec5dc
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-10 16:50:26 +0300

    add changes to the file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,6 +12,12 @@ export class QuoteComponent implements OnInit {
   //   new Quote (2, 'Collins Wasonga','If you want to live a happy life,tie it to a goal, not people or things','Albert Einsten',new Date(2020,7,27)),
   //   new Quote (3, 'Ashley Livoga','Never let the fear of striking out keep you from playing the game','Babe Ruth',new Date(2020,9,16)),
   // ];
+  quotes:Quote[] = [
+    {id:1, name:'You only live once, but if you do it right, once is enough'},
+    {id:2,name:'If you want to live a happy life, tie to a goal, not people or things'},
+    {id:3,name:'Never let the fear of striking out keep you from playing the game'},
+  
+  ];
 
   constructor() { }
 
