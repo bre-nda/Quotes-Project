f10e543b3a7e54a008795be2c5b16fefe74b6e0b
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-11 00:26:31 +0300

    add delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
 
   // ];
   quotes:Quote[] = [
-    new Quote(1,'You only live once, but if you do it right, once is enough','Mae West','Brenda Andeso',new Date(2020,4,9)),
+    new Quote(1,'You only live once, but if you do it right, once is enough','Mae West','Brenda Andeso',new Date(2017,4,9)),
     new Quote(2,'If you want to live a happy life, tie to a goal, not people or things','Albert Eintsen','Collins Wasonga',new Date(2019,4,10,)),
     new Quote(3,'Never let the fear of striking out keep you from playing the game','Babe Ruth','Ashley Liavoga',new Date(2018,5,11)),
   ];
